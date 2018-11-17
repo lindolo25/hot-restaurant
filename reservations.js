006696f52f5b613b4e7d2cfbcf461b8243a16a56
lindolo25 <lindolo25@outlook.com>
2018-11-17 13:13:04 -0500

    some changes to reservation.js

--- reservations.js
+++ reservations.js
@@ -4,15 +4,10 @@
 // Constructor function for creating Movie objects
 var Table = function(ID, Name, Email, Phone) {
   // this.cast will hold all of our reservation objects
-  this.table = [];
+  this.ID = ID;
   this.Name = Name;
   this.Email = Email;
   this.Phone = Phone;
-
-  // This method that creates a CastMember object from the constructor function we required and pushes it to the `this.table` array
-  this.addreservation = function(ID, Name, Email, Phone) {
-    this.cast.push(new reservation(ID, Name, Email, Phone));
-  };
 };
 
 // Exporting the Movie constructor which we will use in server.js
